866de6b504176df277fcb2dd3b3dc93d3ea86e07
Yann Hodique <hodiquey@vmware.com>
2014-11-26 13:29:08 -0800

    exec to get rid of bash process

--- proxy-amb.sh
+++ proxy-amb.sh
@@ -2,4 +2,4 @@
 
 SERV=$1
 PORT=$2
-/opt/nsproxy/nsproxy -a 0.0.0.0:${PORT} -p /services/${SERV} -C http://${ETCD_PORT_10000_TCP_ADDR}:${ETCD_PORT_10000_TCP_PORT}
+exec /opt/nsproxy/nsproxy -a 0.0.0.0:${PORT} -p /services/${SERV} -C http://${ETCD_PORT_10000_TCP_ADDR}:${ETCD_PORT_10000_TCP_PORT}
